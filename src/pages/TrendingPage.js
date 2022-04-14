c691e02f6cfefe0aceb37fe345733dbe80b08e69
Mstian <1404746239@qq.com>
2022-04-14 23:45:24 +0800

    feat: update trending page

--- src/pages/TrendingPage.js
+++ src/pages/TrendingPage.js
@@ -1,14 +1,26 @@
 import React, {Component} from "react";
 import { StyleSheet, View, Text } from "react-native";
+import NavigationBar from 'react-native-navbar-plus';
+import keys from '../res/data/langs.json';
+import { tabNav } from "../navigator/NavigationDelegate";
 export default class Index extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>趋势</Text>
+                <NavigationBar title='趋势' />
+                {
+                    keys.length ? tabNav({Component: TrendingTab, theme: {themeColor: '#2196f3'}, keys}) : null
+                }
             </View>    
         );
     }
 }
+
+function TrendingTab(props) {
+    const {tabLabel} = props;
+    return <Text>{tabLabel + 'dadada'}</Text>;
+}
+
 
 const styles = StyleSheet.create({
     container: {
